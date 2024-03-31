6cc2b871af708ac000b3f73567457cf485d077a5
KOMAX233 <fajasmine@example.com>
2024-03-31 14:49:33 -0400

    fixed shapelist flex and 26

--- a4/a4-preact/src/ShapeList.tsx
+++ a4/a4-preact/src/ShapeList.tsx
@@ -5,12 +5,14 @@ import * as State from "./state";
 
 export default function ShapeList() {
     return (
-        <div class="p-[10px] w-full h-full bg-[white] flex flex-wrap gap-x-[10px] gap-y-[10px]"
+        <div class="p-[10px] w-full h-full bg-[white] "
         onClick={State.deSelectAll}
         >
-            {State.shapes.value.map((shape) => (
-                <Shape key={shape.id} shape={shape} />
-            ))}
+            <div class="flex-1 flex flex-wrap gap-x-[10px] gap-y-[10px]">
+                {State.shapes.value.map((shape) => (
+                    <Shape key={shape.id} shape={shape} />
+                ))}
+            </div>
         </div>
     );
 }
